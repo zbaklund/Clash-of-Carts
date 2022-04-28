4c2fc852d1db7de1b7a451359ca834a3cd3bb76c
Zach <caday33@gmail.com>
2022-04-28 00:00:03 -0500

    map changes (got rid of all the high poly assets) and organization cleanup

---
+++ Assets/Scripts/ColorShift.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorShift : MonoBehaviour
+{
+    // Interpolate light color between two colors back and forth
+    float duration = 1.0f;
+    public Color color0;
+    public Color color1;
+
+    Light lt;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        // set light color
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        lt.color = Color.Lerp(color0, color1, t);
+    }
+}
