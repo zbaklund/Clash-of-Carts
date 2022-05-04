68b2b45cd963c0d2cc0b5d4c538e7e996e4a21a4
Mick Ohtake <mikado98765@gmail.com>
2022-05-03 23:00:44 -0500

    plus extra package files

--- Assets/Scripts/PowerUpController.cs
+++ Assets/Scripts/PowerUpController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public enum PowerUpTypes {
     speedBoost,
@@ -9,13 +10,17 @@ public enum PowerUpTypes {
 
 public class PowerUpController : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other) {
-        string powerUpName;
-        if (other.gameObject.tag == "Player") {
-            // TODO: something here to update player inventory
-            // other.gameObject.GetComponent<XRPlayerController>().updateInventory(powerUpName)
-            Destroy(this.gameObject);
-        }
+    // private void OnTriggerEnter(Collider other) {
+    //     string powerUpName;
+    //     if (other.gameObject.tag == "Player") {
+    //         // TODO: something here to update player inventory
+    //         // other.gameObject.GetComponent<XRPlayerController>().updateInventory(powerUpName)
+    //         Destroy(this.gameObject);
+    //     }
+    // }
+
+    public void duhduh() {
+        Debug.Log("GRABBED INTERACTABLE");
     }
     
 }
